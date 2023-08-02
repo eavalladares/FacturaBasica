ee077be6e57d504bf32b3018d052a14c0c736627
Alexander Valladares <edwin.valladares23@itca.edu.sv>
2023-08-02 08:54:39 -0600

    Solicita datos de productos
    Solicita datos de productos e imprime total con impuestos

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿String nombreProducto;
+int cantidadxProducto, cantidadDeProductos = 10;
+double precio, total, totalAcumulado = 0;
+
+for (int i = 1; i <= cantidadDeProductos; i++)
+{
+    Console.WriteLine("Nombre de producto " + i + ":");
+    nombreProducto = Console.ReadLine();
+
+    Console.WriteLine("Cantidad de productos" + i + ":");
+    cantidadxProducto = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Precio por producto " + i + ":");
+    precio = double.Parse(Console.ReadLine());
+
+    total = (cantidadxProducto * precio) * 1.13;
+    totalAcumulado = totalAcumulado + total;
+    Console.Clear();
+
+}
+
+Console.WriteLine("El precio total a pagar es: $" + double.Round(totalAcumulado, 2));
